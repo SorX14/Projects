0f91873bd60852c4d200f2110ef374f65624d2d4
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2016-11-23 18:07:05 +0000

    Add functions for line & signal attenuation
    Add new functions to fetch line and signal attenuation figures.

--- HomeHub5/HomeHub5.php
+++ HomeHub5/HomeHub5.php
@@ -308,6 +308,60 @@ class HomeHub5 extends Utils {
 	}
 	
 	/**
+	*   Attempt to find the line attenuation figures, and return them as they appear
+	*
+	*       @param string $body The body of the document
+	*       @return array The received and transmitted values
+	**/
+	public function getLineAttenuation($body) {
+		// Looks for the line attenuation row and gets the text before the end of the cell tag
+		if (preg_match('%9. Line attenuation:</td>.*?>(?<data>.*?)</%si', $body, $matches)) {
+			// Explode the results and check that we have the correct number of items
+			$e = explode(' / ', $matches['data']);
+			if (count($e) == 2) {
+				// Return raw values
+				// We'll return as an object rather than an array to keep it clean for Restler
+				$return = new \stdClass();
+				$return->raw = $matches['data'];
+				$return->actual = (float) $e[0];
+				$return->maximum = (float) $e[1];
+				
+				return $return;
+			}
+			return false;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	*   Attempt to find the signal attenuation figures, and return them as they appear
+	*
+	*       @param string $body The body of the document
+	*       @return array The received and transmitted values
+	**/
+	public function getSignalAttenuation($body) {
+		// Looks for the signal attenuation row and gets the text before the end of the cell tag
+		if (preg_match('%10. Signal attenuation:</td>.*?>(?<data>.*?)</%si', $body, $matches)) {
+			// Explode the results and check that we have the correct number of items
+			$e = explode(' / ', $matches['data']);
+			if (count($e) == 2) {
+				// Return raw values
+				// We'll return as an object rather than an array to keep it clean for Restler
+				$return = new \stdClass();
+				$return->raw = $matches['data'];
+				$return->actual = (float) $e[0];
+				$return->maximum = (float) $e[1];
+				
+				return $return;
+			}
+			return false;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
 	 * Attempt to find the version number and last update date
 	 * 
 	 * @param string $body The body of the document
